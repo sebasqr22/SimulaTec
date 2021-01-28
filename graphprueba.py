bb277d40f8e751f1d888be7b2f22fb6b34a83d14
Kenichih48 <62965660+Kenichih48@users.noreply.github.com>
2021-01-27 18:08:39 -0600

    Dijsktra + Grafo Implementados
    Co-Authored-By: shaky0907 <62965535+shaky0907@users.noreply.github.com>
    Co-Authored-By: sebasqr22 <63075293+sebasqr22@users.noreply.github.com>

--- graphprueba.py
+++ graphprueba.py
@@ -4,76 +4,39 @@ import random
 #Combinando este con https://www.bogotobogo.com/python/python_Dijkstras_Shortest_Path_Algorithm.php
 #podemos crear el graph con dijsktra
 
-
-
-
-
 #######################################################################################################
-class Vertex:
-
-    def __init__(self, idv):
-        self.id = idv
-        self.adjacent = {}
-
-    def add_Neighbor(self, name, cost = 0):
-        self.adjacent[name] = cost
-
-    def get_Connections(self):
-        return self.adjacent.keys()
-    
-    def get_Id(self):
-        return self.id
-    
-    def set_Previous(self, prev):
-        self.previous = prev
-    
-    def get_Previous(self):
-        return self.previous
-
-    def get_Weight(self, neighbor):
-        return self.adjacent[neighbor]
-
-   
-       
-#######################################################################################################
-
-
 class Graph:
-
     def __init__(self):
         self.v = {}
         self.size = 0
+        self.i = 1
 
     def __iter__(self):
         return iter(self.v.values())
 
     def add_Vertex(self, name):
-        new_vertex = Vertex(name)
-        self.v[name] = new_vertex
-
+        self.v[name] = {}
         self.size += 1
     
     def add_Edge(self, frm, to, res):
+        if to in self.v[frm]:
+            to = str(to) + str(self.i)
+            self.i += 1
+        self.v[frm].update({to: res.get_Res()})
 
-        if frm not in self.v:
-            self.add_Vertex(frm)
-        if to not in self.v:
-            self.add_Vertex(to)
-
-        self.v[frm].add_Neighbor(self.v[to], res.get_Res())
-
-    def Dijsktra_short_path(self,frm):
-        pass
-    
-    def Dijsktra_long_path(self,frm):
-        pass
 
 #######################################################################################################
 
 class Resistance(pygame.sprite.Sprite):
 
-    def __init__(self, idr):
-        self.res = random.randrange(10,100)
+    def __init__(self, idr, res):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprite = None
+        #self.rect = self.sprite.get_rect()
+        self.x = 0
+        self.y = 0
+
+        self.res = res
         self.id = "R" + str(idr)
         self.connectedto = None
         
@@ -99,33 +62,71 @@ class Resistance(pygame.sprite.Sprite):
 #######################################################################################################   
 
 g = Graph()
-
 g.add_Vertex('a')
 g.add_Vertex('b')
 g.add_Vertex('c')
 g.add_Vertex('d')
 
-R1 = Resistance(1)
-R2 = Resistance(2)
-R3 = Resistance(3)
-R4 = Resistance(4)
+R1 = Resistance(1,100)
+R2 = Resistance(2,200)
+R3 = Resistance(3,300)
+R4 = Resistance(4,400)
 
 g.add_Edge('a','b',R1)
 g.add_Edge('a','c',R2)
 g.add_Edge('b','c',R3)
 g.add_Edge('c','d',R4)
 
-print ('Graph data:')
+print(g.v)
+#print ('Graph data:')
 
-for v in g:
+'''for v in g:
     for w in v.get_Connections():
         vid = v.get_Id()
         wid = w.get_Id()
         print (( vid , wid, v.get_Weight(w)))
-ps = 100
-lista = []
-for i in range(0,8):
-    lista.append(ps)
-    ps += 70
+'''
 
-print(lista)
+#graph = {'a':{'b':10,'c':3},'b':{'c':1,'d':2},'c':{'b':4,'d':8,'e':2},'d':{'e':7},'e':{'d':9}}
+#graph = {'a':{'b':8,'f':13,'c':3},'b':{'c':2,'d':1},'c':{'b':3,'d':9,'e':2},'d':{'e':4,'h':2,'g':6},'e':{'a':5,'d':6,'f':5,'i':4},
+#'f':{'i':7,'g':1},'g':{'h':4,'e':3},'h':{'i':1},'i':{'g':5}}
+#print(graph.items())
+
+def dijkstra(graph,start,goal):
+    shortest_distance = {}
+    predecessor = {}
+    unseenNodes = graph.v
+    infinity = 999999
+    path = []
+    for node in unseenNodes:
+        shortest_distance[node] = infinity
+    shortest_distance[start] = 0
+    
+    while unseenNodes:
+        minNode = None
+        for node in unseenNodes:
+            if minNode is None:
+                minNode = node
+            elif shortest_distance[node] < shortest_distance[minNode]:
+                minNode = node
+        for childNode, weight in graph.v[minNode].items():
+            if weight + shortest_distance[minNode] < shortest_distance[childNode]:
+                shortest_distance[childNode] = weight + shortest_distance[minNode]
+                predecessor[childNode] = minNode
+        unseenNodes.pop(minNode)
+    
+    currentNode = goal
+    while currentNode != start:
+        try:
+            path.insert(0,currentNode)
+            currentNode = predecessor[currentNode]
+        except KeyError:
+            print('Path not reachable')
+            break
+    path.insert(0,start)
+    if shortest_distance[goal] != infinity:
+        print('Shortest distance is ' + str(shortest_distance[goal]))
+        print('And the path is ' + str(path))
+
+dijkstra(g, 'a', 'd')
+
